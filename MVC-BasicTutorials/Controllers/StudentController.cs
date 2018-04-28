ce45fcb05e4414869771251f8a6db5718f5d3d8b
Mohamed Younus Hakim <younus.hakkim@gmail.com>
2018-04-28 10:38:02 +0530

    First Commit

---
+++ MVC-BasicTutorials/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_BasicTutorials.Models;
+
+namespace MVC_BasicTutorials.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var studentList = new List<Student>
+            {
+                new Student() { StudentId = 1, StudentName = "Younus", Age = 26},
+                new Student() { StudentId = 2, StudentName = "Balaji", Age = 27 },
+                new Student() { StudentId = 3, StudentName = "Niyaz", Age = 19}
+            };
+
+            //return "This is Index action method of StudentController";
+            return View(studentList);
+        }
+    }
+}
---
+++ MVC-BasicTutorials/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_BasicTutorials.Models
+{
+    public class Student
+    {
+        public int StudentId
+        { get; set; }
+
+        public string StudentName
+        { get; set; }
+
+        public int Age
+        { get; set; }
+    }
+}
